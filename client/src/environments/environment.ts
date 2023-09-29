ea3f659781d8abb38e40f2f07f8b2ab1a39550e7
s2000promax <s2000promax@gmail.com>
2023-09-29 09:23:59 +0400

    feat: change server deploy

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://main--celadon-faun-1beac6.netlify.app/api',
+    apiUrl: 'https://itransition-mgame-server.vercel.app/api',
     gameServerSocketUrl:
-        'https://main--celadon-faun-1beac6.netlify.app/game-platform',
+        'https://itransition-mgame-server.vercel.app/game-platform',
 };
