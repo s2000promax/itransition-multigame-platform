ab229af71a3e7e512bb365e8151bfc04be8349e2
s2000promax <s2000promax@gmail.com>
2023-09-29 08:42:03 +0400

    feat: change server deploy

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://main--celadon-faun-1beac6.netlify.app/api',
     gameServerSocketUrl:
-        'https://main--celadon-faun-1beac6.netlify.app/game-platform',
+        'https://main--celadon-faun-1beac6.netlify.app/game-platform/',
 };
