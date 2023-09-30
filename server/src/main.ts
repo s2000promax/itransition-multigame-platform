e44691ed134b58b0615ffd2d0155d3faaff87f50
s2000promax <s2000promax@gmail.com>
2023-09-30 19:48:18 +0400

    fix: nest deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import appConfig from './config/app/appConfig';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
