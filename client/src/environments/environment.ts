51d12c7bad972c1edb648ff1d2a88b4fb80b6fb7
s2000promax <s2000promax@gmail.com>
2023-09-30 17:47:12 +0400

    fix: Dockerfile

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'http://95.111.247.254/api',
-    gameServerSocketUrl: 'http://95.111.247.254/game-platform',
+    gameServerSocketUrl: 'http://95.111.247.254:3011/game-platform',
 };
